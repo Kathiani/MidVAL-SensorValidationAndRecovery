ff22d6994913c1971c500761970b5f742761b185
Kathiani <kathianisouza@gmail.com>
2024-03-24 21:37:58 -0300

    connected to MongoDB

---
+++ src/main/java/com/example/kathiani/utils/Utilities.java
@@ -0,0 +1,23 @@
+package com.example.kathiani.utils;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Utilities {
+
+    public static void populateDataBase() {
+        String path = "/home/MIdVAL/ManagedData/nomeArquivo.txt";
+        obter o tamanho da colection
+        criar os batches Float batches = a partir do tamanho da colectio
+        batch1 = databasesize/2;
+        batch2 = databasesize/3;
+        batch3 = databasesize/4;
+        batch4 = databasesize/5;
+        
+
+
+        for para o lote batch1
+    }
+    
+}
