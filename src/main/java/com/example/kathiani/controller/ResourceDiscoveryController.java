dfe2fecdd7ab902a50aedca75dffb267ef224961
Kathiani <kathianisouza@gmail.com>
2024-03-30 00:10:35 -0300

    adding function computing metrics

--- src/main/java/com/example/kathiani/controller/ResourceDiscoveryController.java
+++ src/main/java/com/example/kathiani/controller/ResourceDiscoveryController.java
@@ -2,10 +2,12 @@ package com.example.kathiani.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import com.example.kathiani.utils.Utilities;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -19,97 +21,47 @@ import org.springframework.ui.Model;
 @RestController
 public class ResourceDiscoveryController {
 
-        @GetMapping("/midval")
-	    public String controller(Model model) {
-			String jsonResponse = null;
-			String uuid = null;
-			StringBuilder formattedString = null;
-			RestTemplate restTemplate = new RestTemplate();
-	    	String url = "http://10.10.10.104:8000/catalog/resources";
-	        try {
-				jsonResponse = restTemplate.getForObject(url, String.class);		
-				ObjectMapper objectMapper = new ObjectMapper();
-				JsonNode rootNode = objectMapper.readTree(jsonResponse);
-				uuid = rootNode.get("resources").get(0).get("uuid").asText();
-
-				// Imprimir o UUID
-				System.out.println("UUID: " + uuid);
-				
-        	} catch (Exception e) {
-            	e.printStackTrace();
-        	}
-    	    
-	    	String returnErrorData = errorInjectionFunction(uuid);   //Envia os dados do sensor para o injetor de erros LapesFi
-	    	//validatedSensor = validaData(sensorValueError);   //Envia dado para a função de validação
-	    	//sendtoApplication(validatedSensor);  //Envia dado para aplicação 
-	    	//return "logmid";
+       
+	    public void controller(Model model) {
+	    	//String url = "http://10.10.10.104:8000/catalog/resources";       
+	    	//String returnErrorData = errorInjectionFunction("9cf609af-3e7d-4bde-adad-f8b6f2dbe297");   //Cadastra erros em determinado recurso
 			
+			//String validatedSensorResponse = validateData(returnErrorData);   //Envia dado para a função de validação
+			
+	    	//sendtoApplication(validatedSensor);  //Envia dado para aplicação	
+
+		    //Adicionar dados na InterSCity
+		    //ResponseEntity<String> response = Utilities.senddataResource("8520039d-9480-442d-887a-333f0b300bdc");
+	        //return  "UUId do recurso cujo erro será injetado: " + uuid + "\n \n * E * Retorno do LapesFI com dados incorretos: \n" +
+		    //formattedString.toString();		
+	    
+            //return validatedSensorResponse.toString();
+
+		}   
+	
+
+        @GetMapping("/midval/validate")
+		public static String validateData(String returnErrorData) {
+            StringBuilder formattedString = new StringBuilder();
+            //Tentando acessar lotes de dados
 			try {
 				ObjectMapper objectMapper = new ObjectMapper();
 				JsonNode[] jsonNodes = objectMapper.readValue(returnErrorData, JsonNode[].class);
-			    formattedString = new StringBuilder();
-            for (JsonNode node : jsonNodes) {
-                // Verificar se o campo "resource_uuid" da interscity confere com uuid e formatar dados
-                if ("9cf609af-3e7d-4bde-adad-f8b6f2dbe297".equals(node.get("resource_uuid").asText())) {
-                  formattedString.append(node.toString()).append("\n"); // Adicione os dados JSON com uma nova linha
-				  //String resourceUUID = node.get("resource_uuid").asText();
-				  //String resourceValue =  node.get(fieldName) *Tenta guardar a string formatada sem criar o objeto
-           
-                }
-            }
+                for (int i = 0; i < jsonNodes.length; i+=3) {
+           		  JsonNode node = jsonNodes[i];
+                  formattedString.append(node.get(i).toString()).append("\n"); // Adicione os dados JSON com uma nova linha	
+				  formattedString.append(node.get(i+1).toString()).append("\n");
+				  formattedString.append(node.get(i+2).toString()).append("\n");
+				}
+            
 
-        	} catch (Exception e) {
+          	} catch (Exception e) {
           	    e.printStackTrace();
         	}
-			
-			//testar requisição post na interscity
-		   String response = Utilities.populateDatabase();
 
-	    	//return  "UUId do recurso cujo erro será injetado: " + uuid + "\n \n * E * Retorno do LapesFI com dados incorretos: \n" +
-			//formattedString.toString();		
-	    
-          return response;
+			return formattedString.toString();
 
-		}   
-
-		
-		@SuppressWarnings("deprecation")
-		public String errorInjectionFunction(String uuid) {
-			RestTemplate restTemplate = new RestTemplate();
-			String endpointUrl = "http://localhost:4000/interscity";  // Acessando LapesFI
-
-			// Obter a hora atual do sistema
-     		LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-            String horaAtualFormatada = now.format(formatter);
-			
-			String jsonString = String.format(
-            "{" +
-                "\"uuid\": \"%s\"," +
-                "\"type_of_error\": \"loss accuracy\"," +
-                "\"initial_date\": \"%s\"," +
-                "\"final_date\": \"%s\"," +
-                "\"intensity\": 129" +
-            "}",
-            uuid,horaAtualFormatada, // Adicionando hora atual formatada
-                horaAtualFormatada);
-		
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.APPLICATION_JSON);
-			HttpEntity<String> requestEntity = new HttpEntity<>(jsonString, headers);
-	        ResponseEntity<String> responseEntity = restTemplate.postForEntity(endpointUrl, requestEntity, String.class); //post
-
-	            if (responseEntity.getStatusCode().is2xxSuccessful()) {
-	                System.out.println("Erro injetado na capability!");
-	            } else {
-	                System.out.println("Erro ao injetar o erro nos dados: " + responseEntity.getStatusCodeValue());
-	            }
-				;
-
-            String responseErrorInjection = restTemplate.getForObject(endpointUrl, String.class); //
-			return responseErrorInjection;
-
-	        }
+		}
 }
         
 
--- src/main/java/com/example/kathiani/utils/Utilities.java
+++ src/main/java/com/example/kathiani/utils/Utilities.java
@@ -1,146 +1,57 @@
-package com.example.kathiani.utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class Utilities {
+     public String errorInjectionFunction(String uuid) {
+			RestTemplate restTemplate = new RestTemplate();
+			String endpointUrl = "http://localhost:4000/interscity";  // Acessando LapesFI
 
-   public static String populateDatabase(){
-      String uuid = null;
-      //ResponseEntity<String> responseEntity = createCapability();
-      //ResponseEntity<String> responseEntity = createResource();
-      ResponseEntity<String> responseEntity = senddataResource(uuid);
-      return responseEntity.toString();
-         
-    }
-
-    public static ResponseEntity<String> createCapability(){
-      String jsonBody = "{\n" +
-                  "  \"name\": \"temperature_and_humidity_monitoring\",\n" +
-                  "  \"description\": \"Measure the temperature and humidity of the environment\",\n" +
-                  "  \"capability_type\": \"sensor\"\n" +
-                  "}";
-
-      RestTemplate restTemplate = new RestTemplate();
-      String endpointUrl = "http://10.10.10.104:8000/catalog/capabilities"; 
-      HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
-	   ResponseEntity<String> responseEntity = restTemplate.postForEntity(endpointUrl, requestEntity, String.class);
-
-        System.out.println("Olá, mundo!");
-        
-         if (responseEntity.getStatusCode().is2xxSuccessful()) {
-	                System.out.println("Capacidade criada com sucesso!");
-	            } else {
-	                System.out.println("Erro ao criar a capacidade: " + responseEntity.getStatusCode());
-	            }
-				;
-
-        return responseEntity;
-    }
-
-    public static ResponseEntity<String> createResource(){
-      String jsonBody = "{\n" +
-                  "  \"data\": {\n" +
-                  "    \"description\": \"Um sensor de temperatura e umidade no campus da UFSCar\",\n" +
-                  "    \"capabilities\": [\n" +
-                  "      \"environment_monitoring\"\n" +
-                  "    ],\n" +
-                  "    \"status\": \"active\",\n" +
-                  "    \"lat\": -23.559616,\n" +
-                  "    \"lon\": -46.731386\n" +
-                  "  }\n" +
-                  "}";
-      
-      RestTemplate restTemplate = new RestTemplate();
-      String endpointUrl =  "http://10.10.10.104:8000/adaptor/resources";
-      HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
-	   ResponseEntity<String> responseEntity = restTemplate.postForEntity(endpointUrl, requestEntity, String.class);
-      if (responseEntity.getStatusCode().is2xxSuccessful()) {
-	                System.out.println("Recurso criado com sucesso!");
-	   } else {
-	                System.out.println("Erro ao criar recurso: " + responseEntity.getStatusCode());
-	      };
-				
-
-      return responseEntity;
-    }
-
-    public static ResponseEntity<String> senddataResource(String uuid){  
-      ResponseEntity<String> responseEntity = ResponseEntity.ok().body("");
-     
-      int collectioninterval = 1000; //em segundos
-      int batch = 10;
-
-      for (int i = 0; i < batch; i++) {
-         double temperature = generateTemperature();
-         double humidity = generateHumidity();
-         
-         LocalDateTime now = LocalDateTime.now();
-         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-         String horaAtual = now.format(formatter);
-
-         String jsonBody= String.format(
-            "{" +
-               "\"data\": {" +
-                  "\"environment_monitoring\": [" +
-                        "{" +
-                           "\"temperature\": \"%f\"," + 
-                           "\"humidity\": \"%f\"," + 
-                           "\"timestamp\": \"%s\"," + 
-                        "}" +
-                  "]" +
-               "}" +
-            "}", temperature, humidity, horaAtual);
-            
-
-         RestTemplate restTemplate = new RestTemplate();
-         String endpointUrl =  "http://10.10.10.104:8000/adaptor/resources/" + uuid + "/data";
-         HttpHeaders headers = new HttpHeaders();
-	   	headers.setContentType(MediaType.APPLICATION_JSON);
-
-         try {
-               Thread.sleep(collectioninterval);
-         } catch (InterruptedException e) {
-        // Tratamento da exceção aqui, se necessário
-          e.printStackTrace(); // ou qualquer outra ação desejada
-         }
-
-         HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
-	      responseEntity = restTemplate.postForEntity(endpointUrl, requestEntity, String.class);
-         if (responseEntity.getStatusCode().is2xxSuccessful()) {
-	                System.out.println("Dado adicionado com sucesso!");
-	      } else {
-	                System.out.println("Erro ao adicionar dado: " + responseEntity.getStatusCode());
-	      };
-        
-      }
-         
-      return responseEntity;
+			// Obter a hora atual do sistema
+     		LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            String horaAtualFormatada = now.format(formatter);
 			
-      
-   }
+			String jsonString = String.format(
+            "{" +
+                "\"uuid\": \"%s\"," +
+                "\"type_of_error\": \"loss accuracy\"," +
+                "\"initial_date\": \"%s\"," +
+                "\"final_date\": \"%s\"," +
+                "\"intensity\": 129" +
+            "}",
+            uuid,horaAtualFormatada, 
+                horaAtualFormatada); // String formatada para requisição
+		
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<String> requestEntity = new HttpEntity<>(jsonString, headers);
+	        ResponseEntity<String> responseEntity = restTemplate.postForEntity(endpointUrl, requestEntity, String.class); //requisição post criando erro no recurso com determinado uuid 
+	        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+	                System.out.println("Erro injetado no recurso!");
+	            } else {
+	                System.out.println("Erro ao injetar o erro no recurso: " + responseEntity.getStatusCodeValue());
+	        }
+			;
 
-      public static double generateTemperature() {
-        // Simplesmente gera um valor aleatório entre 0 e 5 para a temperatura
-        return Math.random() * 5;
-    }
+            String responseErrorInjection = restTemplate.getForObject(endpointUrl, String.class); //Obter resposta com dados injetados
+			return responseErrorInjection;
 
-    public static double generateHumidity() {
-        // Simplesmente gera um valor aleatório entre 0 e 5 para a temperatura
-        return Math.random() * 5;
-    }
+	        }
+
+			public static void computeMetrics(){
+				
+			}
+
+}
 
 		
 
    
     
-}
+
